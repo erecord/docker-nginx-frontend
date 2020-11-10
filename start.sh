c4d1b47798a70116c1dbad2c5f94fd7a18e6ba8e
root <root@localhost.localdomain>
2020-11-10 19:50:49 +0000

    Removed start.sh script

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -e ./nginx/default.conf.example ] && [ ! -e ./nginx/default.conf ];
-then
-  cp ./nginx/default.conf.example ./nginx/default.conf
-fi
-
-
-docker-compose restart $1
